91c769b6ff58badea997871bcaab12bf3fa50a57
fahadmoidheen <82438383+fahadmoidheen@users.noreply.github.com>
2021-05-09 21:10:31 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -4,13 +4,18 @@ function ajax() {
         if(this.readyState==4&&this.status==200){
             var resp =JSON.parse(this.responseText);
             var htmlContent =""
-            for(var i =0; i<resp.length; i++){
-                var currentRecord = resp[i];
-                htmlContent += "<tr><td>"+currentRecord.id+"</td><td>"+currentRecord.title+"</td><td><input type='checkbox' name='test' value="+currentRecord.completed+"></td></tr>"
-            };
-            document.getElementById("tableContents").innerHTML = htmlContent;
+            for (var i=0;i<resp.length;i++){
+                var currentData =resp[i];
+                htmlContent+= `<tr><td>${currentData.id}</td><td>${currentData.title}</td><td><input type="checkbox" ${checkValue(currentData.completed)} value=${currentData.title}</td></tr>`
+              };
+              document.getElementById("tableContents").innerHTML= htmlContent;
+                }
+              
             
-        }
+            function checkValue(value1){
+              console.log(value1);
+              if(value1){return "disabled"}  else{return null;}
+            };
     }
     xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
     xhttp.send();
